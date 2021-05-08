72b272f81c58e0fe3a0f0347347377282f40ea7a
Ara Kris Utucu <arautucu@sabanciuniv.edu>
2021-05-08 18:57:18 +0300

    Script.js changed

--- chrome-extension(front-end)/script.js
+++ chrome-extension(front-end)/script.js
@@ -18,11 +18,11 @@ chrome.tabs.query(
 
     chrome.i18n.detectLanguage(title, function (result) {
       //var outputLang = result.languages[0].language;
-      var outputLang = '';
-      for(i = 0; i < result.languages.length; i++) {
+      var outputLang = "";
+      for (i = 0; i < result.languages.length; i++) {
         outputLang += result.languages[i].language + "";
       }
-      
+
       var myDict = {
         en: "English",
         tr: "Turkish",
@@ -40,9 +40,9 @@ chrome.tabs.query(
       } else {
         outputLang2 = outputLang;
       }
-      
 
-      document.getElementById("language").innerHTML = outputLang2 + "<br>" + "\nReliability: " + result.isReliable;
+      document.getElementById("language").innerHTML =
+        outputLang2 + "<br>" + "\nReliability: " + result.isReliable;
       if (outputLang2 !== "English" && outputLang2 !== "Turkish") {
         document.getElementById("btnNer").style.display = "none";
       }
@@ -51,54 +51,59 @@ chrome.tabs.query(
 );
 
 window.onload = function () {
-  document.getElementById("btnKeyword").addEventListener("click", function () {
-    if (!document.getElementById("keywordLabel")) {
-      var labelKeyword = document.createElement("label");
-      labelKeyword.setAttribute("for", "keyword");
-      labelKeyword.innerText = "Keyword:";
-      labelKeyword.setAttribute("id", "keywordLabel");
-      labelKeyword.setAttribute("style", "font-weight: bold;");
-      document.getElementById("caseSensID").appendChild(labelKeyword);
+  var element = document.getElementById("btnKeyword");
+  if (element !== null) {
+    document
+      .getElementById("btnKeyword")
+      .addEventListener("click", function () {
+        if (!document.getElementById("keywordLabel")) {
+          var labelKeyword = document.createElement("label");
+          labelKeyword.setAttribute("for", "keyword");
+          labelKeyword.innerText = "Keyword:";
+          labelKeyword.setAttribute("id", "keywordLabel");
+          labelKeyword.setAttribute("style", "font-weight: bold;");
+          document.getElementById("caseSensID").appendChild(labelKeyword);
 
-      var inputKeyword = document.createElement("input");
-      inputKeyword.setAttribute("type", "text");
-      inputKeyword.setAttribute("id", "keyword");
-      inputKeyword.setAttribute("name", "keyword");
-      document.getElementById("caseSensID").appendChild(inputKeyword);
+          var inputKeyword = document.createElement("input");
+          inputKeyword.setAttribute("type", "text");
+          inputKeyword.setAttribute("id", "keyword");
+          inputKeyword.setAttribute("name", "keyword");
+          document.getElementById("caseSensID").appendChild(inputKeyword);
 
-      var inputCheckbox = document.createElement("input");
-      inputCheckbox.setAttribute("type", "checkbox");
-      inputCheckbox.setAttribute("name", "caseSens");
-      inputCheckbox.setAttribute("value", "1");
-      inputCheckbox.setAttribute("id", "caseSens");
-      inputCheckbox.setAttribute("checked", "checked");
-      document.getElementById("caseSensID").appendChild(inputCheckbox);
+          var inputCheckbox = document.createElement("input");
+          inputCheckbox.setAttribute("type", "checkbox");
+          inputCheckbox.setAttribute("name", "caseSens");
+          inputCheckbox.setAttribute("value", "1");
+          inputCheckbox.setAttribute("id", "caseSens");
+          inputCheckbox.setAttribute("checked", "checked");
+          document.getElementById("caseSensID").appendChild(inputCheckbox);
 
-      var labelCheckbox = document.createElement("label");
-      labelCheckbox.setAttribute("for", "caseSens");
-      labelCheckbox.setAttribute("style", "color: blue;");
-      labelCheckbox.setAttribute("id", "checkBoxLabel");
-      labelCheckbox.innerHTML = "Case Sensitive";
-      document.getElementById("caseSensID").appendChild(labelCheckbox);
-    } else {
-      document.getElementById("keyword").style.display = "inline";
-      document.getElementById("caseSens").style.display = "inline";
-      document.getElementById("checkBoxLabel").style.display = "inline";
-      document.getElementById("keywordLabel").style.display = "inline";
+          var labelCheckbox = document.createElement("label");
+          labelCheckbox.setAttribute("for", "caseSens");
+          labelCheckbox.setAttribute("style", "color: blue;");
+          labelCheckbox.setAttribute("id", "checkBoxLabel");
+          labelCheckbox.innerHTML = "Case Sensitive";
+          document.getElementById("caseSensID").appendChild(labelCheckbox);
+        } else {
+          document.getElementById("keyword").style.display = "inline";
+          document.getElementById("caseSens").style.display = "inline";
+          document.getElementById("checkBoxLabel").style.display = "inline";
+          document.getElementById("keywordLabel").style.display = "inline";
+        }
+      });
+
+    var elems = document.getElementsByClassName("buttons");
+    for (var i = elems.length; i--; ) {
+      elems[i].addEventListener("click", fn, false);
     }
-  });
 
-  var elems = document.getElementsByClassName("buttons");
-  for (var i = elems.length; i--; ) {
-    elems[i].addEventListener("click", fn, false);
-  }
-
-  function fn() {
-    if (this.innerText !== "Keyword") {
-      document.getElementById("keyword").style.display = "none";
-      document.getElementById("caseSens").style.display = "none";
-      document.getElementById("checkBoxLabel").style.display = "none";
-      document.getElementById("keywordLabel").style.display = "none";
+    function fn() {
+      if (this.innerText !== "Keyword") {
+        document.getElementById("keyword").style.display = "none";
+        document.getElementById("caseSens").style.display = "none";
+        document.getElementById("checkBoxLabel").style.display = "none";
+        document.getElementById("keywordLabel").style.display = "none";
+      }
     }
   }
 };
