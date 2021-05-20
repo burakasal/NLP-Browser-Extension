3943e2e29231c6418015108240c5a691c371f5b6
Burak Asal <asalburak@sabanciuniv.edu>
2021-05-20 12:34:28 +0300

    update

--- back-end/GetData.py
+++ back-end/GetData.py
@@ -7,12 +7,12 @@ def getData(array):
     data = request.get_data()
     data2 = data.decode("ascii")
     html_content = requests.get(data2).text
-    soup = BeautifulSoup(html_content, 'lxml')
+    soup = BeautifulSoup(html_content, 'lxml') #Parsing
     for a in soup.findAll('a', href=True):
-        a.decompose()
+        a.decompose() #Remove all 'a' tags which contain links
     footer_tag = soup.footer
     if footer_tag == True:
-        footer_tag.decompose()
+        footer_tag.decompose() #Remove footer tags
     mytext = soup.find_all("p")
     if(array):
         text = []
--- back-end/OCR.py
+++ back-end/OCR.py
@@ -12,25 +12,25 @@ def ocr():
     data2 = data.decode("ascii")
     html_content = requests.get(data2).text
     soup = BeautifulSoup(html_content, 'lxml')
-    images = soup.find_all("img")
+    images = soup.find_all("img") #Find all image tags
     image_list = []
     for image in images:
         a = str(image)
 
-        if ".png" in a or ".svg" in a or ".jpg" in a:
-            image_list.append(image["src"])
+        if ".png" in a or ".svg" in a or ".jpg" in a: #Check if it is a valid image
+            image_list.append(image["src"]) #Append the src to the list which contains the image link
             
     term = "<ul>"
     
     for i in image_list:
         try:
-            if "http" not in i:
+            if "http" not in i: #Http is added to the link if it does not exist, for get request
                 i="http:" +i
             response = requests.get(i)
             img = Image.open(io.BytesIO(response.content))
             a = pytesseract.image_to_string(img)
             if a.strip():
-                #a=re.sub(r'[^A-Za-z0-9]', "", a)
+                a=re.sub(r'[^A-Za-z0-9]', "", a)
                 term+="<li>"+ a+ "</li>"
         except:
             continue       
