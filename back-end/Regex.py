ef136c0fb04f85d1e2902f6b515896d3cb5fe043
zyaradanakul <zyaradanakul@sabanciuniv.edu>
2021-05-21 16:12:50 +0300

    ENS492PROJECT

--- back-end/Regex.py
+++ back-end/Regex.py
@@ -1,8 +1,6 @@
 import re
 
-def regex(text, data2): 
-    #text is the webpage content, data2 is the regular expression input
-
+def regex(text, data2):  #text is the webpage content, data2 is the regular expression input
     data2= r'' + data2 
     result=re.findall(data2, text)
 
@@ -12,17 +10,15 @@ def regex(text, data2):
         return result
 
     term = "<ul>" #these tags are added to represent the outcome as bulletpoints in the output box.
-    
-    #in case there is a wrong input entrance, it is checked whether the resulting match is nonempty, empty matches are not included in the output
-    for i in result:
+
+    for i in result: #in case there is a wrong input entrance, it is checked whether the resulting match is nonempty, empty matches are not included in the output
         i=i.strip()
         if len(i)>0:
             term+="<li>" + i +"</li>"
     
     term += "</ul>"
     
-    #if all of the matches are empty, there is no match
-    if term=="<ul></ul>":
+    if term=="<ul></ul>": #if all of the matches are empty, there is no match
         term="Expression is not found"
 
     return term
